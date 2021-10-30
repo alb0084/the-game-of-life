902f531190bf67b1755d623ebb391286b98e0b0c
Alberto <alberto.cerea84@gmail.com>
2021-10-30 23:47:48 +0200

    Added transition modal effect

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,5 @@
 import ReactDom from "react-dom";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../ButtonAction/ButtonAction";
 import { useContext } from "react";
 import ResultsContext from "../../store/Result-context";
@@ -8,6 +8,7 @@ import { modal } from "../../models/Modal";
 import { NAME_CLICK, TYPE_BUTTON, TITLE_MODAL_BUTTON, H2_TITLE, MESSAGE_OVERLAY, SUGGESTION} from "../../helpers/Costants";
 import styles2 from "../ButtonAction/ButtonAction.module.css";
 import styles from "./Modal.module.css";
+import { Transition } from "react-transition-group";
 
 const Backdrop:React.FC<modal> = (props: any): JSX.Element => {
   return <div className={styles.backdrop} onClick={props.onConfirm}></div>;
@@ -15,28 +16,37 @@ const Backdrop:React.FC<modal> = (props: any): JSX.Element => {
 
 const ModalOverlay:React.FC<modal> = (props: any): JSX.Element => {
   const ctx: any = useContext(ResultsContext);
-  const refreshPage = (): void => window.location.reload();
-
   const maxRound = ctx.round;
+  const refreshPage = (): void => window.location.reload();
+  const [modal,setModal] = useState(false);
+  useEffect(()=>{
+    ctx.gameOver && setModal(ctx.gameOver);
+  },[ctx.gameOver])
   return (
     <>
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{H2_TITLE}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{MESSAGE_OVERLAY + ` ${maxRound}, ` + SUGGESTION}</p>
+      <Transition in={modal} timeout={200}>
+        {state=>(
+        <div style={{ opacity: state==='entered'? 1: 0, transition:'opacity 200ms linear' }}>
+          <Card className={styles.modal}>
+            <header className={styles.header}>
+              <h2>{H2_TITLE}</h2>
+            </header>
+            <div className={styles.content}>
+              <p>{MESSAGE_OVERLAY + ` ${maxRound}, ` + SUGGESTION}</p>
+            </div>
+            <footer className={styles.actions}>
+              <Button
+                type={TYPE_BUTTON}
+                name={NAME_CLICK}
+                value={TITLE_MODAL_BUTTON}
+                styles={styles2["pure-material-button-contained"]}
+                setValAction={refreshPage}
+              ></Button>
+            </footer>
+          </Card>
         </div>
-        <footer className={styles.actions}>
-          <Button
-            type={TYPE_BUTTON}
-            name={NAME_CLICK}
-            value={TITLE_MODAL_BUTTON}
-            styles={styles2["pure-material-button-contained"]}
-            setValAction={refreshPage}
-          ></Button>
-        </footer>
-      </Card>
+        )}
+      </Transition>
     </>
   );
 };
